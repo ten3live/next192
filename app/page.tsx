04461f1c86cd8ef6abc27c2d5bf50705489b42c0
Sajjad Ali <ten3live@gmail.com>
2026-01-13 14:22:49 +0500

    Database Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import prisma from '@/lib/prisma'
+import Link from 'next/link';
 
-function page() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <div>Latest Next Working</div>
-  )
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
+      <Link className='btn btn-primary' href={'/posts'}>Posts</Link>
+    </div>
+  );
 }
-
-export default page
